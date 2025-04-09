c76baf70229281f4832d002d0d4ef8c358f321b1
Murilo R R <130069044+Murilo-R-Robetti@users.noreply.github.com>
2025-04-09 11:31:12 -0300

    tá indo

---
+++ script.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 8086;
+
+app.get('/', function(req, res){
+    res.send("Bem-vindo!");
+});
+
+app.get('/homepage', function(req, res, next){
+    console.log("A resposta estÃ¡ na prÃ³xima funÃ§Ã£o.");
+    next();
+}, (req, res) => {
+    res.send("Bem-vindo ao homepage!");
+});
+
+//Passagem por parÃ¢metro
+app.get('/ola/:nome/:sobrenome', function(req, res){
+    res.send(`Bem-vindo, ${req.params.nome} ${req.params.sobrenome}`);
+});
+
+//Passagem por parÃ¢metro usando Query String
+//localhost:8086/ola?nome=Leandro&sobrenome=Ensina
+app.get('/ola', function(req, res){
+    const {nome, sobrenome} = req.query
+    res.send(`Bem-vindo, ${nome} ${sobrenome}`);
+});
+
+app.get('/titulo', function(req, res){
+    fetch("https://brasilapi.com.br/api/isbn/v1/{isbn}" + "Akira")
+        .then((response) => response.json())
+        .then((titulo) => {
+            res.send(`Titulo: ${titulo.title}`);
+        })
+        .catch(error => {
+            console.log("Erro ao acessar o link");
+            res.send("Ops, houve um erro.");
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}...`)
+});
